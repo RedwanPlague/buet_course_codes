231be6c35dc8dc4cb450b0069123bfee60f714d7
RedwanPlague <mdredwan.rh@gmail.com>
2020-10-20 22:23:28 +0600

    deleted redundant files/folders. added lines-of-action folter to ai-lab

---
+++ L3T2/ai-lab/line-of-action-game/one.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+
+    int val = 0;
+    for (int i=0; i<10; i++) {
+        cout << val << endl;
+        cin >> val;
+        val += 2;
+    }
+
+    return 0;
+}
